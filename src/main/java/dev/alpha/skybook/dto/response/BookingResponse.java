47b81733e88e6d35f56098a1bf16d1eca0afe2cb
MUNEZERO Alpha <alphamnzr@gmail.com>
2026-08-21 08:42:38 +0200

    updeted the response in dto

--- src/main/java/dev/alpha/skybook/dto/response/BookingResponse.java
+++ src/main/java/dev/alpha/skybook/dto/response/BookingResponse.java
@@ -3,14 +3,9 @@ package dev.alpha.skybook.dto.response;
 import dev.alpha.skybook.enums.BookingStatus;
 
 public record BookingResponse(
-
         Long id,
-
         Long passengerId,
-
         Long flightId,
-
         BookingStatus status
-
 ) {
 }
